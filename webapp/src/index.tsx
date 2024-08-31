99d3c2cd2b112c5ea54f9d3162f9f7df746d7da1
JAY-KANANI-18 <jaykanani1999@gmail.com>
2024-08-31 16:46:24 +0530

    test

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -12,14 +12,34 @@ import { Provider } from 'react-redux';
 import React, { useState, useEffect } from 'react';
 
 
+const myMiddleware = (store:any) => (next:any) => (action:any) => {
+    console.log('Dispatching action:', action);
+
+    // Example: Only allow actions of a specific type
+    if (action.type === 'MY_CUSTOM_ACTION') {
+        // Do something with the action or state
+    }
+
+    // Proceed to the next middleware or reducer
+    return next(action);
+};
 export default class Plugin {
+
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
     initialize(registry:any, store:any) {
         // Register a custom sidebar button
+        const customMiddleware = myMiddleware;
+
         console.log('Store:', store);
         console.log('Store State:', store.getState());
         console.log('Dispatch Method:', store.dispatch);
         console.log('Subscribe Method:', store.subscribe);
+        const dispatch = store.dispatch;
+        store.dispatch = (action:any) => {
+            // Apply your middleware to each dispatch
+            return customMiddleware(store)(dispatch)(action);
+        };
         registry.registerLeftSidebarHeaderComponent(  SidebarButton);
     }
 
