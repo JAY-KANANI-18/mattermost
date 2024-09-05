5a7e759fed14bb6ca7cc201332e8ded43cd6fd5d
JAY-KANANI-18 <jaykanani1999@gmail.com>
2024-09-05 10:16:54 +0530

    change 1st one

--- webapp/src/components/interceptor.tsx
+++ webapp/src/components/interceptor.tsx
@@ -4,46 +4,44 @@ const InterceptorComponent: React.FC = () => {
     useEffect(() => {
         // Save the original fetch function
         const originalFetch = window.fetch;
-        type Args = [RequestInfo, RequestInit?];
 
         // Override the global fetch function
         window.fetch = async (...args: [RequestInfo | URL, RequestInit?]) => {
-            // Call the original fetch function
             let url: string;
 
+            // Determine if args[0] is a string, Request, or URL object
             if (typeof args[0] === 'string') {
                 url = args[0];
             } else if (args[0] instanceof Request) {
-                url = args[0].url;
+                url = args[0].url;  // Extract URL from Request object
+            } else if (args[0] instanceof URL) {
+                url = args[0].toString();  // Convert URL to string
+                args[0] = url;  // Update args[0] to be a string
             } else {
-                // If neither a string nor a Request object, call the original fetch
                 return originalFetch(...args);
             }
 
+            // Call the original fetch
             const response = await originalFetch(...args);
 
             // Clone the response to modify it
             const clonedResponse = response.clone();
 
             // Check if the URL includes the specific path
-            const data = await clonedResponse.json();
-            console.log({data});
             if (url.includes('/api/v4/users')) {
-
-
-
+                const data = await clonedResponse.json();
 
                 // Modify the response data
-                // data.customField = 'Modified Data';
+                data.customField = 'Modified Data';
 
-                // // Create a new Response object with modified data
-                // const modifiedResponse = new Response(JSON.stringify(data), {
-                //     status: response.status,
-                //     statusText: response.statusText,
-                //     headers: response.headers,
-                // });
+                // Create a new Response object with modified data
+                const modifiedResponse = new Response(JSON.stringify(data), {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: response.headers,
+                });
 
-                return data;
+                return modifiedResponse;
             }
 
             return response;
